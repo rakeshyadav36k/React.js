5c2b5154723dd1a7e9dc0080688d71495a113bb5
Rakesh Yadav <rakeshyadav36k@gmail.com>
2024-11-10 16:46:42 +0530

    Create first react app

---
+++ 01vitereact/src/App.jsx
@@ -0,0 +1,13 @@
+
+
+
+
+function App() {
+
+
+  return (
+      <h1>Hello React with vite</h1>
+  )
+}
+
+export default App
---
+++ 01vitereact/src/main.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import ReactDom from 'react-dom/client'
+import App from './App.jsx'
+
+
+ReactDom.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+)
+
+// import { StrictMode } from 'react'
+// import { createRoot } from 'react-dom/client'
+// import App from './App.jsx'
+
+
+// createRoot(document.getElementById('root')).render(
+//   <StrictMode>
+//     <App />
+//   </StrictMode>,
+// )
