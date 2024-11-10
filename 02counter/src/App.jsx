1750e9cf7cd37aaec700fe8fdbff8bf01f2d30e7
Rakesh Yadav <rakeshyadav36k@gmail.com>
2024-11-10 16:51:18 +0530

    Create counter app

---
+++ 02counter/src/App.jsx
@@ -0,0 +1,36 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+
+  let [counter, setCounter] = useState(15)
+
+  // let counter = 15
+
+  const addValue = () =>{
+    console.log("clicked", counter);
+    // counter = counter + 1;
+    if(counter < 20) setCounter(counter + 1)
+
+  }
+  const removeValue = () =>{
+    console.log("clicked", counter);
+    // counter = counter + 1;
+    if(counter >= 1) setCounter(counter - 1)
+
+  }
+
+  return (
+    <>
+      <h1> Chai aur code</h1>
+      <h2>Counter value : {counter}</h2>
+
+      <button onClick={addValue}>Add value {counter}</button>
+      <button onClick={removeValue}>Remove value {counter}</button>
+    </>
+  )
+}
+
+export default App
