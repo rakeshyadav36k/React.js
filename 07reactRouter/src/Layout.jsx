b2cb9d4b2342dccfdd83246069c7b88fa05a49a6
Rakesh Yadav <rakeshyadav36k@gmail.com>
2024-11-18 23:33:12 +0530

    Update Layout.jsx

--- 07reactRouter/src/Layout.jsx
+++ 07reactRouter/src/Layout.jsx
@@ -2,14 +2,14 @@ import React from 'react'
 import { Outlet } from 'react-router-dom'
 import Home from './components/Home/Home'
 import Footer from './components/Footer/Footer'
-function Layout() {
+function Layout() {// Home and footer component will not change while navigating from one page to another
   return (
     <>
       <Home />
-      <Outlet />
+      <Outlet />   
       <Footer />
     </>
   )
-}
+} 
 
 export default Layout
